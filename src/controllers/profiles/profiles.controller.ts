90412d3757f423aa3ef8525e74089c4dcf40eeda
AlonaPiasetskaDev <alona.piasetska.dev@gmail.com>
2022-02-04 15:43:17 +0200

    change controller

--- src/controllers/profiles/profiles.controller.ts
+++ src/controllers/profiles/profiles.controller.ts
@@ -3,38 +3,58 @@ import { CreateProfileDto, UpdateProfileDto } from './../../dtos/profiles.dto';
 import { Controller, Get, Post, Param, Body, Put, Delete, Res, HttpStatus } from '@nestjs/common';
 import { Observable, of } from 'rxjs';
 import { Response } from 'express';
+import { ProfilesService } from 'src/providers/profiles/profiles.service';
+import { IProfile } from 'src/interfaces/profile.interface';
 
 @Controller('profiles')
 export class ProfilesController {
 
-  @Post()
-  createProfile(@Body() createProfileDto: CreateProfileDto, @Res() res: Response){
-      console.log(createProfileDto);
-      res.status(HttpStatus.CREATED).send();
-      return 'This action adds a new profile';
-  }
 
-  @Get()
-  getProfiles(@Res() res: Response): Observable<any[]> {
-    console.log('This action returns all profiles');
-    res.status(HttpStatus.OK).json([]);
-    return of([]);
-  }
+    constructor(private profilesService: ProfilesService){}
 
-  @Get(':id')
-  getProfile(@Param() params): string {
-    console.log(params.id);
-    return `This action returns a #${params.id} cat`;
-  }
+    @Post()
+    async createProfile(@Body() createProfileDto:CreateProfileDto) {
+        this.profilesService.createProfile(createProfileDto);
+    }
 
-  @Put(':id')
-  updateProfile(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto){
-    console.log(updateProfileDto);
-    return `This action updates  a ${id} cat`;
-  }
+    @Get()
+    async getProfiles(): Promise<IProfile[]> {
+        return this.profilesService.getProfiles()
+    }
+    // TODO: convert promise to observable
+
+
+
+
+
+//   @Post()
+//   createProfile(@Body() createProfileDto: CreateProfileDto, @Res() res: Response){
+//       console.log(createProfileDto);
+//       res.status(HttpStatus.CREATED).send();
+//       return 'This action adds a new profile';
+//   }
+
+//   @Get()
+//   getProfiles(@Res() res: Response): Observable<any[]> {
+//     console.log('This action returns all profiles');
+//     res.status(HttpStatus.OK).json([]);
+//     return of([]);
+//   }
+
+//   @Get(':id')
+//   getProfile(@Param() params): string {
+//     console.log(params.id);
+//     return `This action returns a #${params.id} cat`;
+//   }
+
+//   @Put(':id')
+//   updateProfile(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto){
+//     console.log(updateProfileDto);
+//     return `This action updates  a ${id} cat`;
+//   }
   
-  @Delete(':id')
-  deleteProfile(@Param('id') id: string) {
-      return `This action remove a ${id} profile`;
-  }
+//   @Delete(':id')
+//   deleteProfile(@Param('id') id: string) {
+//       return `This action remove a ${id} profile`;
+//   }
 }
