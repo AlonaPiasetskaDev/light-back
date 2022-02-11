df97c200a36d2b2df272e66fafd145fd8125c8e2
AlonaPiasetskaDev <alona.piasetska.dev@gmail.com>
2022-02-11 07:44:31 +0200

    auth

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { UserModule } from './user/user.module';
+import { ProfileModule } from './profile/profile.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: ['http://localhost:4201', 'http://localhost:4200'] });
+  app.useGlobalPipes(new ValidationPipe({
+    // disableErrorMessages: true,
+  }));
 
-  const config = new DocumentBuilder()
+  const config = new DocumentBuilder().addBearerAuth()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [
+      UserModule,
+      ProfileModule,
+    ],
+  });
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(4201);
 }
+
 bootstrap();
+
---
+++ src/user/user.controller.ts
@@ -0,0 +1,96 @@
+import { Roles } from '../auth/decorators/roles.decorator';
+import { ResetPasswordDto } from './dto/reset-password.dto';
+import { CreateForgotPasswordDto } from './dto/create-forgot-password.dto';
+import { Request } from 'express';
+import { LoginUserDto } from './dto/login-user.dto';
+import { Controller, Get, Post, Body, UseGuards, Req, HttpCode, HttpStatus } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { VerifyUuidDto } from './dto/verify-uuid.dto';
+import { UserService } from './user.service';
+import { AuthGuard, PassportModule } from '@nestjs/passport';
+import { RefreshAccessTokenDto } from './dto/refresh-access-token.dto';
+import {
+    ApiCreatedResponse,
+    ApiOkResponse,
+    ApiTags,
+    ApiBearerAuth,
+    ApiHeader,
+    } from '@nestjs/swagger';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+
+@ApiTags('User')
+@Controller('api/user')
+@UseGuards(RolesGuard)
+export class UserController {
+    constructor(
+        private readonly userService: UserService,
+        ) {}
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @ApiCreatedResponse({})
+    async register(@Body() createUserDto: CreateUserDto) {
+        return await this.userService.create(createUserDto);
+    }
+
+    @Post('verify-email')
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({})
+    async verifyEmail(@Req() req: Request, @Body() verifyUuidDto: VerifyUuidDto) {
+        return await this.userService.verifyEmail(req, verifyUuidDto);
+    }
+
+    @Post('signin')
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({})
+    async login(@Req() req: Request, @Body() loginUserDto: LoginUserDto) {
+        return await this.userService.login(req, loginUserDto);
+    }
+
+    @Post('refresh-access-token')
+    @HttpCode(HttpStatus.CREATED)
+    @ApiCreatedResponse({})
+    async refreshAccessToken(@Body() refreshAccessTokenDto: RefreshAccessTokenDto) {
+        return await this.userService.refreshAccessToken(refreshAccessTokenDto);
+    }
+
+    @Post('forgot-password')
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({})
+    async forgotPassword(@Req() req: Request, @Body() createForgotPasswordDto: CreateForgotPasswordDto) {
+        return await this.userService.forgotPassword(req, createForgotPasswordDto);
+    }
+
+    @Post('forgot-password-verify')
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({})
+    async forgotPasswordVerify(@Req() req: Request, @Body() verifyUuidDto: VerifyUuidDto) {
+        return await this.userService.forgotPasswordVerify(req, verifyUuidDto);
+    }
+
+    @Post('reset-password')
+    @HttpCode(HttpStatus.OK)
+    @ApiBearerAuth()
+    @ApiHeader({
+        name: 'Bearer',
+        description: 'the token we need for auth.'
+    })
+    @ApiOkResponse({})
+    async resetPassword(@Body() resetPasswordDto: ResetPasswordDto) {
+        return await this.userService.resetPassword(resetPasswordDto);
+    }
+
+    @Get('data')
+    @UseGuards(AuthGuard('jwt'))
+    @Roles('admin', 'users')
+    @ApiBearerAuth()
+    @ApiHeader({
+        name: 'Bearer',
+        description: 'the token we need for auth.'
+    })
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({})
+    findAll() {
+        return this.userService.findAll();
+    }
+}
